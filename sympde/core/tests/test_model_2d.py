f5722f8263ef9b28e0f2d45fc439b43efacb430a
ratnania <ratnaniahmed@gmail.com>
2018-09-17 00:45:11 +0200

    2d model latex test

--- sympde/core/tests/test_model_2d.py
+++ sympde/core/tests/test_model_2d.py
@@ -10,6 +10,7 @@ from sympde.core import TestFunction
 from sympde.core import VectorTestFunction
 from sympde.core import Field
 from sympde.core import Domain
+from sympde.core import Constant
 
 DIM = 2
 
@@ -40,7 +41,66 @@ def test_model_2d_1():
     model.preview(outputTexFile='test_model_2d_1.tex')
 # ...
 
+# ...
+def test_model_2d_2():
+    print('============ test_model_2d_2 ==============')
+
+    domain = Domain('\Omega', dim=DIM)
+
+    V = FunctionSpace('V', domain)
+    x,y = V.coordinates
+
+    u = TestFunction(V, name='u')
+    v = TestFunction(V, name='v')
+    v_1 = TestFunction(V, name='v_1')
+    v_2 = TestFunction(V, name='v_2')
+    v_3 = TestFunction(V, name='v_3')
+    v_4 = TestFunction(V, name='v_4')
+
+    psi = TestFunction(V, name='psi')
+    phi = TestFunction(V, name='phi')
+    w   = TestFunction(V, name='omega')
+    j   = TestFunction(V, name='J')
+
+    psi_n = Field('psi_n',   V)
+    phi_n = Field('phi_n',   V)
+    w_n   = Field('omega_n', V)
+    j_n   = Field('J_n',     V)
+    j_c   = Field('J_c',     V)
+
+    nu  = Constant('nu')
+    eta = Constant('eta')
+    dt  = Constant('dt')
+
+    expr = psi*v - dt*bracket(psi,phi_n)*v #+ dt*nu*dot(grad(psi), grad(v))
+    a_1 = BilinearForm((v,psi), expr, name='a_1')
+
+    expr = (w*v -
+            dt*bracket(w,phi_n)*v +
+            dt*bracket(psi,j_n)*v +
+            dt*eta*dot(grad(psi), grad(v)))
+    a_2 = BilinearForm((v,w), expr, name='a_2')
+
+    m = BilinearForm((v,u), u*v, name='m')
+
+    expr = dot(grad(phi), grad(v))
+    a_4 = BilinearForm((v,phi), expr, name='a_4')
+
+    f = Function('f')
+    b = LinearForm(v, f(x,y)*v, name='b')
+
+    expr = a_1(v_1, psi) + m(v_1, j) + a_2(v_2, w) + m(v_3, j) + a_4(v_4, phi)
+    a = BilinearForm(((v_1, v_2, v_3, v_4),(psi, w, j, phi)), expr, name='a')
+
+    forms = [m, a_1, a_2, a_4, a, b]
+    equation = Equation(a(v,psi), b(v))
+
+    model = Model(domain, forms=forms, equation=equation)
+    model.preview(outputTexFile='test_model_2d_2.tex')
+# ...
+
 # .....................................................
 if __name__ == '__main__':
 
-    test_model_2d_1()
+#    test_model_2d_1()
+    test_model_2d_2()
