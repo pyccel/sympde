cbda0c4790e748a9d439f62c1583b778fea7b5cb
ratnania <ratnaniahmed@gmail.com>
2019-01-14 16:02:31 +0100

    start new module for calculus + identities assertions

---
+++ sympde/calculus/core.py
@@ -0,0 +1,417 @@
+# coding: utf-8
+
+from sympy.core.compatibility import is_sequence
+from sympy.core import Basic
+from sympy import Indexed, IndexedBase
+from sympy.core import Add, Mul, Pow
+from sympy.core.containers import Tuple
+from sympy.core.singleton import S
+
+from sympde.core.basic import CalculusFunction
+from sympde.core.basic import _coeffs_registery
+
+from sympde.topology.space import TestFunction, VectorTestFunction, IndexedTestTrial
+from sympde.topology.space import Field, VectorField, IndexedVectorField
+
+
+#==============================================================================
+class BasicOperator(CalculusFunction):
+
+    def __getitem__(self, indices, **kw_args):
+        if is_sequence(indices):
+            # Special case needed because M[*my_tuple] is a syntax error.
+            return Indexed(self, *indices, **kw_args)
+        else:
+            return Indexed(self, indices, **kw_args)
+
+#==============================================================================
+class Dot(BasicOperator):
+
+    def __new__(cls, *args, **options):
+        # (Try to) sympify args first
+
+        if options.pop('evaluate', True):
+            r = cls.eval(*args)
+        else:
+            r = None
+
+        if r is None:
+            return Basic.__new__(cls, *args, **options)
+        else:
+            return r
+
+    @classmethod
+    def eval(cls, *_args):
+        """."""
+
+        if not _args:
+            return
+
+        if not len(_args) == 2:
+            raise ValueError('Expecting two arguments')
+
+        a,b = _args
+        if (a == 0) or (b == 0):
+            return 0
+
+        return cls(a, b, evaluate=False)
+
+#==============================================================================
+class Cross(BasicOperator):
+    pass
+
+#==============================================================================
+class Inner(BasicOperator):
+    pass
+
+#==============================================================================
+# TODO add it to evaluation
+# Convect(F, G) = dot(F, nabla) G
+class Convect(BasicOperator):
+    pass
+
+#==============================================================================
+class Grad(BasicOperator):
+
+    def __new__(cls, *args, **options):
+        # (Try to) sympify args first
+
+        if options.pop('evaluate', True):
+            r = cls.eval(*args)
+        else:
+            r = None
+
+        if r is None:
+            return Basic.__new__(cls, *args, **options)
+        else:
+            return r
+
+    @classmethod
+    def eval(cls, *_args):
+        """."""
+
+        if not _args:
+            return
+
+        if not len(_args) == 1:
+            raise ValueError('Expecting one argument')
+
+        expr = _args[0]
+        if isinstance(expr, Add):
+            args = expr.args
+            args = [cls.eval(a) for a in expr.args]
+            return Add(*args)
+
+        elif isinstance(expr, Mul):
+            coeffs  = [a for a in expr.args if isinstance(a, _coeffs_registery)]
+            vectors = [a for a in expr.args if not(a in coeffs)]
+
+            a = S.One
+            if coeffs:
+                a = Mul(*coeffs)
+
+            b = S.One
+            if vectors:
+                if len(vectors) == 1:
+                    f = vectors[0]
+                    b = cls(f)
+
+                elif len(vectors) == 2:
+                    f,g = vectors
+                    b = f*cls(g) + g*cls(f)
+
+                else:
+                    left = vectors[0]
+                    right = Mul(*vectors[1:])
+
+                    f_left  = cls(left, evaluate=True)
+                    f_right = cls(right, evaluate=True)
+
+                    b = left * f_right + f_left * right
+
+            return Mul(a, b)
+
+        elif isinstance(expr, Pow):
+            b = expr.base
+            e = expr.exp
+            return e*cls(b)*Pow(b, e-1)
+
+        elif isinstance(expr, Dot):
+            a,b = expr._args
+            return Cross(a, Curl(b)) - Cross(Curl(a), b) + Convect(a,b) + Convect(b,a)
+
+        return cls(expr, evaluate=False)
+
+#==============================================================================
+class Curl(BasicOperator):
+
+    def __new__(cls, *args, **options):
+        # (Try to) sympify args first
+
+        if options.pop('evaluate', True):
+            r = cls.eval(*args)
+        else:
+            r = None
+
+        if r is None:
+            return Basic.__new__(cls, *args, **options)
+        else:
+            return r
+
+    @classmethod
+    def eval(cls, *_args):
+        """."""
+
+        if not _args:
+            return
+
+        if not len(_args) == 1:
+            raise ValueError('Expecting one argument')
+
+        expr = _args[0]
+        if isinstance(expr, Add):
+            args = expr.args
+            args = [cls.eval(a) for a in expr.args]
+            return Add(*args)
+
+        elif isinstance(expr, Mul):
+            coeffs  = [a for a in expr.args if isinstance(a, _coeffs_registery)]
+            vectors = [a for a in expr.args if not(a in coeffs)]
+
+            a = S.One
+            if coeffs:
+                a = Mul(*coeffs)
+
+            b = S.One
+            if vectors:
+                if len(vectors) == 2:
+                    a,b = vectors
+                    if isinstance(a, (Tuple, VectorTestFunction, VectorField,
+                                      Grad)):
+                        f = b ; F = a
+                        return f*Curl(F) + Cross(grad(f), F)
+
+                    elif isinstance(b, (Tuple, VectorTestFunction, VectorField,
+                                       Grad)):
+                        f = a ; F = b
+                        return f*Curl(F) + Cross(grad(f), F)
+
+                b = cls(Mul(*vectors), evaluate=False)
+
+            return Mul(a, b)
+
+        elif isinstance(expr, Cross):
+            a,b = expr._args
+            return a * Div(b) - b*Div(a) + Convect(b, a) - Convect(a, b)
+
+        elif isinstance(expr, Grad):
+            return 0
+
+        elif isinstance(expr, Curl):
+            f = expr._args[0]
+            return Grad(Div(f)) - Laplace(f)
+
+        return cls(expr, evaluate=False)
+
+#==============================================================================
+class Rot(BasicOperator):
+
+    def __new__(cls, *args, **options):
+        # (Try to) sympify args first
+
+        if options.pop('evaluate', True):
+            r = cls.eval(*args)
+        else:
+            r = None
+
+        if r is None:
+            return Basic.__new__(cls, *args, **options)
+        else:
+            return r
+
+    @classmethod
+    def eval(cls, *_args):
+        """."""
+
+        if not _args:
+            return
+
+        if not len(_args) == 1:
+            raise ValueError('Expecting one argument')
+
+        expr = _args[0]
+        if isinstance(expr, Add):
+            args = expr.args
+            args = [cls.eval(a) for a in expr.args]
+            return Add(*args)
+
+        elif isinstance(expr, Mul):
+            coeffs  = [a for a in expr.args if isinstance(a, _coeffs_registery)]
+            vectors = [a for a in expr.args if not(a in coeffs)]
+
+            a = S.One
+            if coeffs:
+                a = Mul(*coeffs)
+
+            b = S.One
+            if vectors:
+                b = cls(Mul(*vectors), evaluate=False)
+
+            return Mul(a, b)
+
+        return cls(expr, evaluate=False)
+
+#==============================================================================
+class Div(BasicOperator):
+
+    def __new__(cls, *args, **options):
+        # (Try to) sympify args first
+
+        if options.pop('evaluate', True):
+            r = cls.eval(*args)
+        else:
+            r = None
+
+        if r is None:
+            return Basic.__new__(cls, *args, **options)
+        else:
+            return r
+
+    @classmethod
+    def eval(cls, *_args):
+        """."""
+
+        if not _args:
+            return
+
+        if not len(_args) == 1:
+            raise ValueError('Expecting one argument')
+
+        expr = _args[0]
+        if isinstance(expr, Add):
+            args = expr.args
+            args = [cls.eval(a) for a in expr.args]
+            return Add(*args)
+
+        elif isinstance(expr, Mul):
+            coeffs  = [a for a in expr.args if isinstance(a, _coeffs_registery)]
+            vectors = [a for a in expr.args if not(a in coeffs)]
+
+            a = S.One
+            if coeffs:
+                a = Mul(*coeffs)
+
+            b = S.One
+            if vectors:
+                if len(vectors) == 2:
+                    a,b = vectors
+                    if isinstance(a, (Tuple, VectorTestFunction, VectorField)):
+                        f = b ; F = a
+                        return f*Div(F) + Dot(F, grad(f))
+
+                    elif isinstance(b, (Tuple, VectorTestFunction, VectorField)):
+                        f = a ; F = b
+                        return f*Div(F) + Dot(F, grad(f))
+
+                b = cls(Mul(*vectors), evaluate=False)
+
+            return Mul(a, b)
+
+        elif isinstance(expr, Cross):
+            a,b = expr._args
+            return Dot(b, Curl(a)) - Dot(a, Curl(b))
+
+        elif isinstance(expr, Curl):
+            return 0
+
+        return cls(expr, evaluate=False)
+
+#==============================================================================
+class Laplace(BasicOperator):
+
+    def __new__(cls, *args, **options):
+        # (Try to) sympify args first
+
+        if options.pop('evaluate', True):
+            r = cls.eval(*args)
+        else:
+            r = None
+
+        if r is None:
+            return Basic.__new__(cls, *args, **options)
+        else:
+            return r
+
+    @classmethod
+    def eval(cls, *_args):
+        """."""
+
+        if not _args:
+            return
+
+        if not len(_args) == 1:
+            raise ValueError('Expecting one argument')
+
+        expr = _args[0]
+        if isinstance(expr, Add):
+            args = expr.args
+            args = [cls.eval(a) for a in expr.args]
+            return Add(*args)
+
+        elif isinstance(expr, Mul):
+            coeffs  = [a for a in expr.args if isinstance(a, _coeffs_registery)]
+            vectors = [a for a in expr.args if not(a in coeffs)]
+
+            a = S.One
+            if coeffs:
+                a = Mul(*coeffs)
+
+            b = S.One
+            if vectors:
+                if len(vectors) == 1:
+                    f = vectors[0]
+                    b = cls(f)
+
+                elif len(vectors) == 2:
+                    f,g = vectors
+                    b = f*cls(g) + g*cls(f) + 2 * Dot(Grad(f), Grad(g))
+
+                else:
+                    b = cls(Mul(*vectors), evaluate=False)
+
+            return Mul(a, b)
+
+        return cls(expr, evaluate=False)
+
+#==============================================================================
+class Bracket(BasicOperator):
+    pass
+
+#==============================================================================
+class Hessian(BasicOperator):
+    pass
+
+Outer = Dot # TODO add the Outer class Function
+
+# ...
+_generic_ops  = (Dot, Cross, Inner, Outer,
+                 Grad, Curl, Rot, Div, Convect,
+                 Bracket, Laplace, Hessian)
+# ...
+
+# ... alias for ufl compatibility
+cross = Cross
+dot = Dot
+inner = Inner
+outer = Outer
+
+grad = Grad
+curl = Curl
+rot = Rot
+div = Div
+
+convect = Convect
+bracket = Bracket
+laplace = Laplace
+hessian = Hessian
+# ...
---
+++ sympde/calculus/tests/test_calculus.py
@@ -0,0 +1,157 @@
+# coding: utf-8
+
+import pytest
+
+from sympy import Symbol
+from sympy.core.containers import Tuple
+from sympy import symbols
+from sympy import IndexedBase
+from sympy import Matrix
+from sympy import Function
+from sympy import pi, cos, sin
+from sympy import srepr
+from sympy import expand
+from sympy.physics.quantum import TensorProduct
+
+from sympde.core import Constant
+from sympde.calculus import grad, dot, inner, cross, rot, curl, div
+from sympde.calculus import laplace, hessian, bracket, convect
+from sympde.topology import Domain
+from sympde.topology import FunctionSpace, VectorFunctionSpace
+from sympde.topology import Field, VectorField
+
+
+#==============================================================================
+def test_calculus_2d():
+    domain = Domain('Omega', dim=2)
+
+    V = FunctionSpace('V', domain)
+    W = VectorFunctionSpace('W', domain)
+
+    alpha, beta, gamma = [Constant(i) for i in ['alpha','beta','gamma']]
+
+    f,g,h = [Field(i, space=V) for i in ['f','g','h']]
+    F,G,H = [VectorField(W, i) for i in ['F','G','H']]
+
+    # ... scalar gradient properties
+    assert( grad(f+g) == grad(f) + grad(g) )
+    assert( grad(alpha*f) == alpha*grad(f) )
+    assert( grad(alpha*f + beta*g) == alpha*grad(f) + beta*grad(g)  )
+
+    assert( grad(f*g) == f*grad(g) + g*grad(f) )
+    assert( grad(f/g) == -f*grad(g)/g**2 + grad(f)/g )
+
+    assert( expand(grad(f*g*h)) == f*g*grad(h) + f*h*grad(g) + g*h*grad(f) )
+    # ...
+
+    # ... vector gradient properties
+    assert( grad(F+G) == grad(F) + grad(G) )
+    assert( grad(alpha*F) == alpha*grad(F) )
+    assert( grad(alpha*F + beta*G) == alpha*grad(F) + beta*grad(G)  )
+
+    assert( grad(dot(F,G)) == convect(F, G) + convect(G, F) + cross(F, curl(G)) - cross(curl(F), G) )
+    # ...
+
+    # ... curl properties
+    assert( curl(f+g) == curl(f) + curl(g) )
+    assert( curl(alpha*f) == alpha*curl(f) )
+    assert( curl(alpha*f + beta*g) == alpha*curl(f) + beta*curl(g)  )
+    # ...
+
+    # ... laplace properties
+    assert( laplace(f+g) == laplace(f) + laplace(g) )
+    assert( laplace(alpha*f) == alpha*laplace(f) )
+    assert( laplace(alpha*f + beta*g) == alpha*laplace(f) + beta*laplace(g)  )
+    # ...
+
+    # ... divergence properties
+    assert( div(F+G) == div(F) + div(G) )
+    assert( div(alpha*F) == alpha*div(F) )
+    assert( div(alpha*F + beta*G) == alpha*div(F) + beta*div(G)  )
+
+    assert( div(cross(F,G)) == -dot(F, curl(G)) + dot(G, curl(F)) )
+    # ...
+
+    # ... rot properties
+    assert( rot(F+G) == rot(F) + rot(G) )
+    assert( rot(alpha*F) == alpha*rot(F) )
+    assert( rot(alpha*F + beta*G) == alpha*rot(F) + beta*rot(G)  )
+    # ...
+
+    expr = grad(dot(F,G))
+    print(expr)
+
+#==============================================================================
+def test_calculus_3d():
+    domain = Domain('Omega', dim=3)
+
+    V = FunctionSpace('V', domain)
+    W = VectorFunctionSpace('W', domain)
+
+    alpha, beta, gamma = [Constant(i) for i in ['alpha','beta','gamma']]
+
+    f,g,h = [Field(i, space=V) for i in ['f','g','h']]
+    F,G,H = [VectorField(W, i) for i in ['F','G','H']]
+
+    # ... gradient properties
+    assert( grad(f+g) == grad(f) + grad(g) )
+    assert( grad(alpha*f) == alpha*grad(f) )
+    assert( grad(alpha*f + beta*g) == alpha*grad(f) + beta*grad(g)  )
+
+    assert( grad(f*g) == f*grad(g) + g*grad(f) )
+    assert( grad(f/g) == -f*grad(g)/g**2 + grad(f)/g )
+
+    assert( expand(grad(f*g*h)) == f*g*grad(h) + f*h*grad(g) + g*h*grad(f) )
+    # ...
+
+    # ... curl properties
+    assert( curl(F+G) == curl(F) + curl(G) )
+    assert( curl(alpha*F) == alpha*curl(F) )
+    assert( curl(alpha*F + beta*G) == alpha*curl(F) + beta*curl(G)  )
+
+    assert( curl(cross(F,G)) == F*div(G) - G*div(F) - convect(F, G) + convect(G, F) )
+    assert( curl(f*F) == f*curl(F) + cross(grad(f), F) )
+    # ...
+
+    # ... laplace properties
+    assert( laplace(f+g) == laplace(f) + laplace(g) )
+    assert( laplace(alpha*f) == alpha*laplace(f) )
+    assert( laplace(alpha*f + beta*g) == alpha*laplace(f) + beta*laplace(g)  )
+    assert( laplace(f*g) == f*laplace(g) + g*laplace(f) + 2*dot(grad(f), grad(g)) )
+    # ...
+
+    # ... divergence properties
+    assert( div(F+G) == div(F) + div(G) )
+    assert( div(alpha*F) == alpha*div(F) )
+    assert( div(alpha*F + beta*G) == alpha*div(F) + beta*div(G)  )
+
+    assert( div(cross(F,G)) == -dot(F, curl(G)) + dot(G, curl(F)) )
+    assert( div(f*F) == f*div(F) + dot(F, grad(f)))
+    # ...
+
+    # ...
+    assert( curl(grad(f)) == 0 )
+    assert( div(curl(F)) == 0 )
+    assert( div(cross(grad(f), grad(g))) == 0 )
+    assert( curl(curl(F)) == grad(div(F)) - laplace(F))
+    assert( curl(f*grad(g)) == cross(grad(f), grad(g)) )
+    # ...
+
+    expr = curl(f*grad(g))
+    print(expr)
+
+
+#==============================================================================
+# CLEAN UP SYMPY NAMESPACE
+#==============================================================================
+
+def teardown_module():
+    from sympy import cache
+    cache.clear_cache()
+
+def teardown_function():
+    from sympy import cache
+    cache.clear_cache()
+
+#test_calculus_2d()
+test_calculus_3d()
