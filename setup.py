8a738f9b0aed04a675a89aaa3f770fc98f5de32b
Yaman Güçlü <yaman.guclu@gmail.com>
2019-08-30 18:21:47 +0200

    Clean up installation procedure with pip (#46)
    * Move dependencies list from 'requirements.txt' to 'setup.py'.
    
    * Move library version to separate file.
    
    NOTE: sympde now provides version to user:
    
    In [1]: import sympde; sympde.__version__
    Out[1]: '0.9.3'
    
    * Update __version__

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 # -*- coding: UTF-8 -*-
 #! /usr/bin/python
 
-import sys
+from pathlib import Path
 from setuptools import setup, find_packages
 
+# ...
+# Read library version into '__version__' variable
+path = Path(__file__).parent / 'sympde' / 'version.py'
+exec(path.read_text())
+# ...
+
 NAME    = 'sympde'
-VERSION = '0.9.3'
+VERSION = __version__
 AUTHOR  = 'Ahmed Ratnani'
 EMAIL   = 'ratnaniahmed@gmail.com'
 URL     = 'https://github.com/pyccel/sympde'
@@ -30,16 +36,21 @@ setup_args = dict(
 packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 # ...
 
-# Requirements from local "requirements.txt" file
-install_requires = []
+# Dependencies
+install_requires = [
+    'sympy>=1.2',
+    'h5py',
+    'pytest',
+    'pyyaml',
+    'yamlloader'
+]
 
 def setup_package():
-    if 'setuptools' in sys.modules:
-        setup_args['install_requires'] = install_requires
 
-    setup(packages = packages, \
-          include_package_data = True, \
-          zip_safe=True, \
+    setup(packages = packages,
+          include_package_data = True,
+          install_requires = install_requires,
+          zip_safe = True,
           **setup_args)
 
 
