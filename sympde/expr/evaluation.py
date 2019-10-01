39200d906827645acec147f6ded12fe7d7b51192
Yaman Güçlü <yaman.guclu@gmail.com>
2019-10-01 16:04:54 +0200

    [codacy] Remove some unused imports.

--- sympde/expr/evaluation.py
+++ sympde/expr/evaluation.py
@@ -1,49 +1,45 @@
 # coding: utf-8
 
-from itertools import groupby
 from itertools import product
 
-from sympy.core import Basic
-from sympy.core import Symbol
-from sympy.core import Function
-from sympy.simplify.simplify import simplify
-from sympy import collect
-from sympy.series.order import Order
-from sympy.core import Expr, Add, Mul, Pow
 from sympy import S
-from sympy.core.containers import Tuple
-from sympy import Indexed, IndexedBase, Matrix, ImmutableDenseMatrix
+from sympy import Indexed, Matrix, ImmutableDenseMatrix
 from sympy import expand
-from sympy import Integer, Float
+from sympy.core import Basic
+from sympy.core import Add, Mul, Pow
 from sympy.core.expr import AtomicExpr
-from sympy.physics.quantum import TensorProduct
-from sympy.series.series import series
+from sympy.core.containers import Tuple
+from sympy.simplify.simplify import simplify
 
 from sympde.core.basic import _coeffs_registery
 from sympde.core.basic import CalculusFunction
-from sympde.core.basic import Constant
 from sympde.core.algebra import (Dot_1d,
-                 Dot_2d, Inner_2d, Cross_2d,
-                 Dot_3d, Inner_3d, Cross_3d)
+                                 Dot_2d, Inner_2d, Cross_2d,
+                                 Dot_3d, Inner_3d, Cross_3d)
 from sympde.core.utils import random_string
 
-from sympde.calculus import Dot, Inner, Cross
-from sympde.calculus import Grad, Rot, Curl, Div
-from sympde.calculus import Bracket
-from sympde.calculus import Laplace
+from sympde.calculus import Dot
+from sympde.calculus import Grad
 from sympde.calculus import jump, avg, minus, plus
-from sympde.calculus import Jump, Average
-from sympde.calculus import NormalDerivative
+from sympde.calculus import Jump
 from sympde.calculus.core import _generic_ops
 
-from sympde.topology import BasicDomain, Domain, MappedDomain, Union, Interval
-from sympde.topology import BoundaryVector, NormalVector, TangentVector
-from sympde.topology import Boundary, Connectivity, Interface
+from sympde.topology import BasicDomain, Union, Interval
+from sympde.topology import NormalVector, TangentVector
+from sympde.topology import Boundary, Interface
 from sympde.topology import InteriorDomain
+from sympde.topology import DetJacobian
+from sympde.topology import SymbolicDeterminant
+from sympde.topology import LogicalExpr
+from sympde.topology.space import ScalarFunctionSpace
+from sympde.topology.space import ScalarTestFunction
+from sympde.topology.space import VectorTestFunction
+from sympde.topology.space import IndexedTestTrial
+from sympde.topology.space import Trace
+from sympde.topology.space import element_of
+from sympde.topology.space import VectorField
 from sympde.topology.derivatives import _partial_derivatives
 from sympde.topology.derivatives import _logical_partial_derivatives
-from sympde.topology.derivatives import partial_derivative_as_symbol
-from sympde.topology.derivatives import sort_partial_derivatives
 from sympde.topology.derivatives import get_atom_derivatives
 from sympde.topology.derivatives import get_atom_logical_derivatives
 from sympde.topology.derivatives import dx, dy, dz
@@ -51,35 +47,15 @@ from sympde.topology.derivatives import dx1, dx2, dx3
 from sympde.topology.derivatives import (Grad_1d, Div_1d,
                                          Grad_2d, Curl_2d, Rot_2d, Div_2d,
                                          Grad_3d, Curl_3d, Div_3d)
-
 from sympde.topology.derivatives import Bracket_2d
 from sympde.topology.derivatives import Laplace_1d, Laplace_2d, Laplace_3d
 from sympde.topology.derivatives import Hessian_1d, Hessian_2d, Hessian_3d
-from sympde.topology.space import BasicFunctionSpace
-from sympde.topology.space import ScalarFunctionSpace
-from sympde.topology.space import ProductSpace
-from sympde.topology.space import ScalarTestFunction
-from sympde.topology.space import VectorTestFunction
-from sympde.topology.space import IndexedTestTrial
-from sympde.topology.space import Unknown, VectorUnknown
-from sympde.topology.space import Trace
-from sympde.topology.space import element_of
-from sympde.topology.space import ScalarField, VectorField, IndexedVectorField
-from sympde.topology.measure import CanonicalMeasure
-from sympde.topology.measure import CartesianMeasure
-from sympde.topology.measure import Measure
-from sympde.topology import Mapping, DetJacobian
-from sympde.topology import SymbolicDeterminant, SymbolicCovariant, SymbolicContravariant
 
-from sympde.topology import LogicalExpr
-
-from .basic  import BasicExpr, BasicForm
-from .expr   import LinearExpr, BilinearExpr
-from .expr   import LinearForm, BilinearForm, Norm
-from .equation import Equation
-from .expr import DomainIntegral, BoundaryIntegral, InterfaceIntegral
-from .expr import Functional
-from .expr import _get_domain
+from .basic import BasicExpr, BasicForm
+from .expr  import BilinearForm
+from .expr  import DomainIntegral, BoundaryIntegral, InterfaceIntegral
+from .expr  import Functional
+from .expr  import _get_domain
 
 #==============================================================================
 def is_sequence(a):
