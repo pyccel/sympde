cfff9fdb9f87d61611d1d59ec8a89b0d368f0417
Yaman Güçlü <yaman.guclu@gmail.com>
2019-11-19 18:26:41 +0100

    Fix order of (trial, test) functions in Equation class.

--- sympde/expr/equation.py
+++ sympde/expr/equation.py
@@ -195,7 +195,7 @@ class Mean(BasicConstraint):
 #        formulation and strong condition
 class Equation(Basic):
 
-    def __new__(cls, lhs, rhs, tests, trials, bc=None, constraint=None):
+    def __new__(cls, lhs, rhs, trials, tests, bc=None, constraint=None):
         # ...
         if not isinstance(lhs, BilinearForm):
             raise UnconsistentLhsError('> lhs must be a bilinear')
@@ -333,11 +333,11 @@ class Equation(Basic):
         # ...
 
         # ... sympify tests/trials
-        tests = Tuple(*tests)
         trials = Tuple(*trials)
+        tests  = Tuple(*tests)
         # ...
 
-        return Basic.__new__(cls, lhs, rhs, tests, trials, bc, constraint)
+        return Basic.__new__(cls, lhs, rhs, trials, tests, bc, constraint)
 
     @property
     def lhs(self):
@@ -373,36 +373,19 @@ class NewtonIteration(Equation):
 
         a = linearize(form, fields, trials=trials)
 
-        trials, tests  = a.variables
+        trials, tests = a.variables
 
         lhs = a
+        rhs = LinearForm(tests, -form.expr)
 
-        form = LinearForm(tests, -form.expr)
-        rhs = form
-
-        return Equation.__new__(cls, lhs, rhs, tests, trials, bc=bc)
+        return Equation.__new__(cls, lhs, rhs, trials, tests, bc=bc)
 
 #==============================================================================
 # user friendly function to create Equation objects
-def find(trials, *, forall, lhs, rhs, **kwargs):
+def find(trials, *, forall, lhs, rhs, bc=None, constraint=None):
 
-    bc = kwargs.pop('bc', None)
-    constraint = kwargs.pop('constraint', None)
+    tests = forall
+    lhs = BilinearForm((trials, tests), lhs)
+    rhs =   LinearForm(         tests , rhs)
 
-    lhs = BilinearForm((trials, forall), lhs)
-    rhs = LinearForm( forall, rhs)
-
-    return Equation(lhs, rhs, forall, trials, bc=bc, constraint=constraint)
-
-#    elif isinstance(lhs, LinearForm):
-#        fields = kwargs.pop('fields', None)
-#        if fields is None:
-#            raise ValueError('Expecting a fields')
-#
-#        rhs = kwargs.pop('rhs', None)
-#        assert((rhs is None) or (rhs == 0))
-#
-#        return NewtonIteration(lhs, fields, bc=bc, trials=trials)
-#
-#    else:
-#        raise NotImplementedError('')
+    return Equation(lhs, rhs, trials, tests, bc=bc, constraint=constraint)
