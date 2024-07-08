108e70b92f74d04e338ac96870d922bcba87597c
Lagarrigue Patrick <patrick.lagarrigue@ensta-paris.fr>
2024-07-08 10:50:44 +0200

    add AnalytciMapping to sympde/topology/__init__.py

--- sympde/topology/__init__.py
+++ sympde/topology/__init__.py
@@ -3,5 +3,6 @@ from .derivatives        import *
 from .datatype           import *
 from .domain             import *
 from .symbolic_mapping   import *
+from .analytical_mappings import *
 from .measure            import *
 from .space              import *
